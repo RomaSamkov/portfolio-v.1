18d3b6d707795d93a42646d72c256a8adabdd48f
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-23 21:24:41 +0200

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import logors from "./logo-color.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -31,6 +32,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  logors,
   logo,
   backend,
   creator,
