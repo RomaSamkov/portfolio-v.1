ca2272fabcd40024d1a6b3fa7db7d95f4941bd32
RomaSamkov <Romansamkov.rs@gmail.com>
2023-04-02 18:57:06 +0300

    add text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -38,11 +38,12 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I am a junior full-stack developer. Looking for a remote full-time job.
+        I am a good team player, as I have a huge experience working with people
+        in a team as well as I am a purposeful and fast learner. Also, I have
+        strong critical-thinking abilities and solid problem-solving skills. I
+        fell in love with coding at the first sight. I want to work in a company
+        that benefits people.
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
