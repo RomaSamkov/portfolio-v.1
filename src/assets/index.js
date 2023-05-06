623cd3e0088efada436d4d87273968f0f8d0ad62
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-06 15:38:23 +0300

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,11 +22,6 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import petsproject from "./petsproject.png";
 import filmoteka from "./filmoteka.png";
 import phonebook from "./phonebook.png";
